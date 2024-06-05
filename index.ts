17bd06908d9a9c1334b39db046f515d668d75249
Saturn <superdinmc@gmail.com>
2024-06-05 12:29:59 +0000

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,142 @@
+declare var self: Worker;
+import fs from 'fs';
+import { availableParallelism } from 'os';
+import { join } from 'path';
+import { parseArgs } from 'util';
+import { Worker, workerData } from 'worker_threads';
+if (Bun.isMainThread) {
+  const args = parseArgs({
+    args: Bun.argv,
+    options: {
+      port: {
+        type: 'string',
+        default: process.env.PORT || '8000',
+        short: 'p'
+      },
+      cluster: {
+        type: 'string',
+        default: availableParallelism().toString(),
+        short: 'c'
+      },
+      help: {
+        type: 'boolean',
+        short: 'h'
+      }
+    },
+    allowPositionals: true
+  })
+  const threadCount = parseInt(`${args?.values?.cluster}`);
+  const port = parseInt(`${args?.values?.port}`);
+  if (!threadCount || Number.isNaN(threadCount)) {
+    console.error('Cluster thread count must be number [--cluster | -c]')
+    process.exit(1);
+  }
+  if (!port || Number.isNaN(port)) {
+    console.error('Port must be number [--port | -p]')
+    process.exit(1);
+  }
+  if (args.values.help) {
+    console.log('Bunginx: Fast, lightweight, simple HTTP file server\n',
+      'bunginx [-phc] [--port <port>] [--cluster <count>] [cwd]\n',
+      '--port <port: number>      Set the port for Bunginx [-p]\n',
+      '                           - defaults to $PORT or 8000.\n',
+      '--cluster <count: number>  Set the threads that Bunginx will spawn [-c]\n',
+      '                           - defaults to the CPU core count.\n',
+      '--help                     Display this message\n',
+      '[cwd: string]              Directory that Bunginx will run on,\n',
+      '                           - defaults to current directory.'
+    )
+    process.exit();
+  }
+  console.log("Starting HTTP cluster of", threadCount, "threads at port", port)
+  for (let i = 0; i < threadCount; i++) {
+    const worker = new Worker(__filename, { workerData: { port, id: i } })
+  }
+} else {
+  var lastpath = 'Unknown Origin';
+  Bun.serve({
+    fetch(req) {
+      const url = new URL(req.url);
+      lastpath = url?.pathname;
+      return resolve(url.pathname);
+    },
+    error(error) {
+      return new Response(`${lastpath}: ${error}`, {
+        status: 500
+      });
+    },
+    reusePort: true,
+    port: workerData?.port
+  });
+
+
+  const notFoundPage = Bun.file(__dirname + '404.html');
+  async function resolve(path: string) {
+    const pathCwd = join(__dirname, path);
+    if (!pathCwd.startsWith(__dirname) || pathCwd.includes('..'))
+      return new Response(path + ': 404 Not Found', {
+        status: 404
+      });
+    if (!fs.existsSync(pathCwd)) {
+      if (await notFoundPage.exists()) return new Response(notFoundPage, {
+        status: 404, headers: new Headers({ 'Content-Type': 'text/html' })
+      });
+      return new Response(path + ': 404 Not Found', {
+        status: 404
+      });
+    }
+    const stat = fs.lstatSync(pathCwd);
+    const indexFile = Bun.file(join(pathCwd, 'index.html'));
+    if (await indexFile.exists()) {
+      if (!path.endsWith('/')) return new Response('', {
+        status: 307,
+        headers: new Headers({
+          'Location': join(path, '/')
+        })
+      })
+      return await resolve(join(path, 'index.html'));
+    }
+    if (stat.isDirectory()) {
+      // Dir
+      const list = fs.readdirSync(pathCwd, { withFileTypes: true }).map(f => {
+        let type = 'Unknown', priority = 0;
+        if (f.isDirectory()) {
+          type = 'Directory';
+          priority = 3;
+          if (fs.existsSync(join(f.name, 'index.html'))) {
+            type = 'Website'
+            priority = 4;
+          }
+        }
+        if (f.isFile()) {
+          const ext = f.name.split('.').slice(1);
+          if (!ext.length) type = 'File';
+          else type = ext.join('.').toUpperCase() + ' file';
+          priority = 2;
+        }
+        if (f.isSymbolicLink()) {
+          type = 'Symlink';
+          priority = 1;
+        }
+        return { name: f.name, type, priority };
+      });
+      return new Response(constructDirView(list, path), {
+        status: 200, headers: new Headers({ 'Content-Type': 'text/html' })
+      });
+    } else {
+      if (stat.mode & (fs.constants.S_IRUSR | fs.constants.S_IRGRP | fs.constants.S_IROTH))
+        return new Response(Bun.file(pathCwd));
+      return new Response(path + ': 403 Forbidden', {
+        status: 403
+      })
+    }
+  }
+  const footer = `<hr><a id="footer">Bunginx | Fast, lightweight, simple HTTP file server<br>Dreamnity inc. 2024</a><style>#footer{font-size:smaller;}#header{font-weight:bold;}#a{white-space:break-spaces;font-family:monospace}*{text-decoration:none}</style>`
+  function constructDirView(list: { name: string, type: string, priority: number }[], url: string) {
+    const maxLength = Math.max(...list.map(e => e.name.length)) + 5;
+    const htmlList = list
+      .sort((i, r) => (r.priority - i.priority) || i.name.localeCompare(r.name))
+      .map(e => `<a id="a" href="${join(url, e.name)}">${e.name.padEnd(maxLength, ' ')} | ${e.type}</a><br>`)
+    return `${url === '/' ? '' : `<a href="${join(url, '..')}"><=</a>`} <a id="header">${url}:</a><br>${htmlList.join('')}${footer}`
+  }
+}
