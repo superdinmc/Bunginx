863ca1d0c34e7500be1793046d4ef40b51388090
Saturn <superdinmc@gmail.com>
2024-06-07 01:56:12 +0000

    remove temporary file

--- runner.js
+++
@@ -1,2 +0,0 @@
-let B=Bun,c=await B.file(B.argv[1]).text();console.log(require('zlib').gunzipSync(Buffer.from(c.slice(c.indexOf('/*@@CODESTART',LEN)+13,-3),'base64url')).toString())
-/*@@CODESTARTHello*/
